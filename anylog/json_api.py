6ff7db38c5fd514748f8674e885ac4c5f19e7f84
Moshe <moshe@anylog.co>
2021-11-16 14:16:48 -0800

    adding json support

---
+++ anylog/json_api.py
@@ -0,0 +1,162 @@
+'''
+By using this source code, you acknowledge that this software in source code form remains a confidential information of AnyLog, Inc.,
+and you shall not transfer it to any other party without AnyLog, Inc.'s prior written consent. You further acknowledge that all right,
+title and interest in and to this source code, and any copies and/or derivatives thereof and all documentation, which describes
+and/or composes such source code or any such derivatives, shall remain the sole and exclusive property of AnyLog, Inc.,
+and you shall not edit, reverse engineer, copy, emulate, create derivatives of, compile or decompile or otherwise tamper or modify
+this source code in any way, or allow others to do so. In the event of any such editing, reverse engineering, copying, emulation,
+creation of derivative, compilation, decompilation, tampering or modification of this source code by you, or any of your affiliates (term
+to be broadly interpreted) you or your such affiliates shall unconditionally assign and transfer any intellectual property created by any
+such non-permitted act to AnyLog, Inc.
+'''
+
+import json
+import sys
+from json.decoder import JSONDecodeError
+
+# -----------------------------------------------------------------------------------
+# Objects to describe a tree hierarchy used in output_tree.html
+# -----------------------------------------------------------------------------------
+class TreeEntry():
+
+    def __init__(self, start_list, end_list, with_children, key, value):
+        self.start_list = start_list     # Start a new list before the print
+        self.end_list = end_list          # end aa list after the print
+        self.with_children = with_children    # With children attributes
+        self.key = key
+        self.value = value
+
+# -----------------------------------------------------------------------------------
+# String to JSON
+# -----------------------------------------------------------------------------------
+def string_to_json( data_str ):
+
+    try:
+        #json_struct = data_str.json()
+        json_struct = json.loads(data_str, encoding="utf8")
+    except ValueError as err:
+        error_msg = "Failed to map string to JSON: %s" % str(err)
+        json_struct = None
+    except JSONDecodeError as err:
+        error_msg = "Failed to map string to JSON: %s" % str(err)
+        json_struct = None
+    except KeyError as err:
+        error_msg = "Failed to map string to JSON: %s" % str(err)
+        json_struct = None
+    except:
+        errno, err = sys.exc_info()[:2]
+        error_msg = "Failed to map string to JSON: %s" % str(err)
+        json_struct = None
+    else:
+        error_msg = None
+
+    return [json_struct, error_msg]
+
+# -----------------------------------------------------------------------------------
+# JSON to string
+# -----------------------------------------------------------------------------------
+def json_to_string(json_struct):
+
+    try:
+        data_str = json.dumps(json_struct)
+    except ValueError as err:
+        error_msg = "Failed to map JSON to string: %s" % str(err)
+        data_str = None
+    except JSONDecodeError as err:
+        error_msg = "Failed to map JSON to string: %s" % str(err)
+        data_str = None
+    except:
+        error_msg = "Failed to map JSON to string"
+        data_str = None
+    else:
+        error_msg = None
+
+    return [data_str, error_msg]
+# =======================================================================================================================
+# String to list
+# =======================================================================================================================
+def string_to_list(data: str):
+
+    try:
+        list_obj = list(eval(data))
+    except:
+        error_msg = "Failed to map List to string"
+        list_obj = None
+    else:
+        error_msg = None
+
+    return [list_obj, error_msg]
+
+# -----------------------------------------------------------------------------------
+# Simple setup of a print of a list of JSON policies
+# Send structure to output.html
+# -----------------------------------------------------------------------------------
+def simple_polisies_list(policies):
+    data_list = []
+    for json_entry in policies:
+        json_string = json.dumps(json_entry,indent=4, separators=(',', ': '), sort_keys=True)
+        data_list.append(json_string)  #  transformed to a JSON string.
+    return data_list
+
+# -----------------------------------------------------------------------------------
+# Print setup of JSON for output_tree.html
+# -----------------------------------------------------------------------------------
+def setup_print_tree( source_struct, print_struct ):
+
+    if isinstance(source_struct, dict):
+
+        counter = len(source_struct) - 1  # The number of entries
+        index = 0
+
+        new_entry = TreeEntry(True, False, False, None, None)  # Start List
+        print_struct.append(new_entry)
+        for key, value in source_struct.items():
+
+            if isinstance(value,list) or isinstance(value, dict):
+                start_list = not counter
+                end_list = counter == index
+                new_entry = TreeEntry(False, False, True, key, None)
+                print_struct.append(new_entry)
+                setup_print_tree( value, print_struct )
+            else:
+                set_edge(key, value, print_struct )
+
+            index += 1
+
+        new_entry = TreeEntry(False, True, False, None, None) # End List
+        print_struct.append(new_entry)
+
+    elif isinstance(source_struct, list):
+        counter = len(source_struct) - 1  # The number of entries
+
+        for index, entry in enumerate(source_struct):
+
+            if isinstance(entry, list) or isinstance(entry, dict):
+                start_list = not counter
+                end_list = counter == index
+                setup_print_tree(entry, print_struct)
+            else:
+                set_edge(None, entry, print_struct)
+
+    else:
+        set_edge(None, source_struct, print_struct)
+
+
+# -----------------------------------------------------------------------------------
+# Add edge Node
+# -----------------------------------------------------------------------------------
+def set_edge(key, value, print_struct):
+
+    if key:
+        data = str(key) + " : " + str(value)
+    else:
+        data = str(value)
+
+    new_entry = TreeEntry(False, False, False, key, value)
+    print_struct.append(new_entry)
+
+
+
+
+
+
--- anylog/views.py
+++ anylog/views.py
@@ -6,7 +6,15 @@ from django.shortcuts import render
 from anylog.forms import AnyLogCredentials
 from django.http import HttpResponse
 
+
+import anylog.json_api as json_api
 import anylog.anylog_conn.anylog_conn as anylog_conn
+
+al_commands = [
+    None,
+    ["get status" , "table"],        # 1
+    ["get event log" , "table"]      # 2
+]
 
 # ---------------------------------------------------------------------------------------
 # GET / POST  AnyLog command form
@@ -22,7 +30,7 @@ def form_request(request):
             node_reply = process_anylog(request)
 
             # print to existing screen content of data (currently DNW)
-            return render(request, "form.html", {'form': user_info, 'node_reply': node_reply})
+            return render(request, "base.html", {'form': user_info, 'node_reply': node_reply})
 
             # print to (new) screen content of data
             # return HttpResponse(data)
@@ -30,7 +38,7 @@ def form_request(request):
         # Display the html form
         user_info = AnyLogCredentials()
 
-        return render(request, "form.html", {'form': user_info})
+        return render(request, "base.html", {'form': user_info})
 
 # ---------------------------------------------------------------------------------------
 # Process the AnyLog command form
@@ -57,3 +65,136 @@ def process_anylog(request):
     output = anylog_conn.get_cmd(conn=conn_info, command=command, authentication=authentication, remote=False)
 
     return output     # Data returned from AnyLog or an Error Message
+
+
+
+# -----------------------------------------------------------------------------------
+# Print network reply -
+# Option 1 - a tree
+# Option 2 - a table
+# Option 3 - text
+# -----------------------------------------------------------------------------------
+def print_network_reply(command, data):
+
+    select_info = get_select_menu()
+    select_info['title'] = 'Network Command'
+    select_info['command'] = command
+
+    policy, table_info, print_info, error_msg = format_message_reply(data)
+    if policy:
+        # Reply was a JSON policy
+        data_list = []
+        json_api.setup_print_tree(policy, data_list)
+        select_info['text'] = data_list
+        # path_selection(parent_menu, id, data)      # save the path, the key and the data on the report
+        return render_template('output_tree.html', **select_info)
+
+    if table_info:
+        # Reply is structured as a table
+        if 'header' in table_info:
+            select_info['header'] = table_info['header']
+        if 'table_title' in table_info:
+            select_info['table_title'] = table_info['table_title']
+        if 'rows' in table_info:
+            select_info['rows'] = table_info['rows']
+        return render_template('output_table.html', **select_info)
+
+    select_info['text'] = print_info        # Only TEXT
+
+    return render_template('output_cmd.html', **select_info)
+
+# -----------------------------------------------------------------------------------
+# Based on the message reply - organize as a table or as an attrubute values list
+# -----------------------------------------------------------------------------------
+def format_message_reply(msg_text):
+    '''
+    Return 4 values depending on the type of message:
+    policy
+    table_info (header, title and rows)
+    Text List (entries are attr - val pairs)
+    '''
+
+    # If the message is a dictionary or a list - return the dictionary or the list
+
+    policy = None
+    error_msg = None
+    if msg_text[0] == '{' and msg_text[-1] == '}':
+        policy, error_msg = json_api.string_to_json(msg_text)
+
+    elif msg_text[0] == '[' and msg_text[-1] == ']':
+        policy, error_msg = json_api.string_to_list(msg_text)
+
+    if policy or error_msg:
+        return [policy, None, None, error_msg]  # return the dictionary or the list
+
+
+    # Make a list of strings to print
+    data = msg_text.replace('\r', '')
+    text_list = data.split('\n')
+
+
+    # Test id the returned message is formatted as a table
+    table_data = {}
+    is_table = False
+    for index, entry in enumerate(text_list):
+        if entry and index:
+            if entry[0] == '-' and entry[-1] == '|':
+                # Identified a table
+                is_table = True
+                columns_list = entry.split('|')
+                columns_size = []
+                for column in columns_list:
+                    if len(column):
+                        columns_size.append(len(column))     # An array with the size of each column
+                header = []
+                offset = 0
+                for column_id, size in enumerate(columns_size):
+                    header.append(text_list[index - 1][offset:offset + size])
+                    offset += (size + 1)                # Add the field size and the separator (|)
+
+                table_data['header'] = header
+                if index > 1 and len(text_list[index -2]):
+                    table_data['table_title'] = text_list[index -2]         # Get the title if available
+                break
+        if index >= 5:
+            break  # not a table
+
+    if is_table:
+        # a Table setup and print
+        table_rows = []
+        for y in range(index + 1, len(text_list)): # Skip the dashed separator to the column titles
+            row = text_list[y]
+
+            columns = []
+            offset = 0
+            for column_id, size in enumerate(columns_size):
+                columns.append(row[offset:offset + size])
+                offset += (size + 1)  # Add the field size and the separator (|)
+
+            table_rows.append(columns)
+
+        table_data['rows'] = table_rows
+        return [None, table_data, None, None]
+
+    # Print Text
+
+    data_list = []     # Every entry holds type of text ("text" or "Url) and the text string
+
+    for entry in text_list:
+        # Setup URL Link (reply to help command + a link to the help page)
+        if entry[:6] == "Link: ":
+            index = entry.rfind('#')  # try to find name of help section
+            if index != -1:
+                section = entry[index + 1:].replace('-', ' ')
+            else:
+                section = ""
+            data_list.append(("url", entry[6:], section))
+        else:
+            # Split text to attribiute value using colon
+            if entry:
+                key_val = entry.split(':', 1)
+                key_val.insert(0, "text")
+
+                data_list.append(key_val)
+
+    return [None, None, data_list, None]
