ef379f98d47107a84c8be569aa016a6cd9a32f3e
Oi Shadmon <oshadmon@gmail.com>
2021-12-04 18:29:57 -0800

    none works

--- anylog_deploy/views.py
+++ anylog_deploy/views.py
@@ -31,12 +31,12 @@ class DeploymentViews:
         :args:
             request:django.core.handlers.wsgi.WSGIRequest - type of request against the form
         :params:
+            config_params:forms.SelectConfig - call to correlated forms
             self.env_params:dict - environment params
             self.config_file:str  - configuration file
                 preset_config_file:str - a config file selected from configs/
                 external_config_file:str - manual input of config file
-            config_params:forms.SelectConfig - call to front page form
-        :redirect:
+        :redriect:
             --> stay - if not is set
             --> deploy_anylog.html - if selecting an existing config file
             --> base_configs.html - if "Configure New Node" button
@@ -59,77 +59,69 @@ class DeploymentViews:
             if config_params.is_valid():
                 preset_config_file = request.POST.get('preset_config_file') # config file in configs/
                 external_config_file = request.POST.get('external_config_file') # manual input of config file
+                external_config_file = os.path.expandvars(os.path.expanduser(external_config_file))
 
-                if preset_config_file != '':
+                if preset_config_file != '' and os.path.isfile(preset_config_file):
                     self.config_file = preset_config_file
-                elif external_config_file != '':
-                    self.config_file = os.path.expandvars(os.path.expanduser(external_config_file))
-                else:
-                    config_params = forms.SelectConfig()
-                    return render(request, "deployment_front_page.html", {'form': config_params})
-
-                if not os.path.isfile(self.config_file):
-                    return render(request, "deployment_front_page.html",
-                                  {'form': config_params, 'node_reply': 'Failed to locate file "%s"' % self.config_file})
-                else:
-                    return HttpResponseRedirect('deploy-anylog/')
-
-        config_params = forms.SelectConfig()
-        if self.config_file is None:
-            return render(request, "deployment_front_page.html", {'form': config_params})
-        if not os.path.isfile(self.config_file):
-            return render(request, "deployment_front_page.html",
-                          {'form': config_params, 'node_reply': 'Failed to locate file "%s"' % self.config_file})
-        else:
+                elif external_config_file != '' and os.path.isfile(external_config_file):
+                    self.config_file = external_config_file
+        if self.config_file is not None:
             return HttpResponseRedirect('deploy-anylog/')
+        else:
+            config_params = forms.SelectConfig()
+            return render(request, "deployment_front_page.html", {'form': config_params})\
 
     def deploy_anylog(self, request)->HttpResponse:
         """
-        Once user either selects a config file or completes config form(s) - user selects what to deploy & executes
+        Once user either selects a config file or completes config form(s) user select what to deploy & executes
         :HTML file:
             deploy_anylog.html
         :args:
             request:django.core.handlers.wsgi.WSGIRequest - type of request against the form
         :params:
+            deploy_config:forms.DeployAnyLog - call to correlated forms
             status:bool - status
             messages:list - list of error messages
-            env_params:dict - params from config file
             update_anylog:bool - whether to update AnyLog (AnyLog gets deployed by default)
-            docker_passwd:str - docker password
-            deploy_config:forms.DeployAnyLog - call to correlated form
-        :redirect:
+            psql:bool - whether to deploy Postgres
+            grafana:bool - whether to deploy Grafana
+        :redriect:
             --> stay w/ message
             --> deployment_front_page.html when pressing "Start Over" button
         """
         status = True
         messages = []
         base_configs = forms.BaseInfo()
-        docker_passwd = None
+        docker_password = None
         update_anylog = False
+        psql = False
+        grafana = False
 
         # Extract configuration information
-        if os.path.isfile(self.config_file): # extract values from config file
-            env_params = io_config.read_configs(config_file=self.config_file)
-        elif all(x == {} for x in self.env_params.values()):
-            return render(request, 'deploy_anylog.html', {'form': base_configs, 'node_reply': 'Missing configurations'})
-        else:
+        if self.config_file is None:
             try:
                 self.config_file = os.path.join(CONFIG_FILE_PATH, '%s.ini' % self.env_params['general']['node_name'])
             except:
-                messages.append('Missing NODE_NAME in set parameters')
+                return render(request, 'deploy_anylog.html',
+                              {'form': base_configs,
+                               'node_reply': 'Missing NODE_NAME in set parameters. Please start over'})
             else:
                 messages = io_config.write_configs(config_data=self.env_params, config_file=self.config_file)
-        if messages != []:
-            return render(request, 'deploy_anylog.html', {'form': base_configs, 'node_reply': messages})
-        else:
-            env_params = io_config.read_configs(config_file=self.config_file)
+                if messages is not None:
+                    return render(request, 'deploy_anylog.html', {'form': base_configs, 'node_reply': messages})
 
-        print(env_params)
+
+        if os.path.isfile(self.config_file): # extract values from config file
+            env_params = io_config.read_configs(config_file=self.config_file)
+            if not io_config.validate_config(env_params=env_params):
+                return render(request, 'deploy_anylog.html',
+                              {'form': base_configs, 'node_reply': 'Failed to validate one or more params'})
+
         # Extract values from forms
         if request.method == 'POST':
             deploy_config = forms.DeployAnyLog(request.POST)
             if deploy_config.is_valid():
-                docker_passwd = request.POST.get('password')
+                docker_password = request.POST.get('password')
                 if request.POST.get('update_anylog') is not None:
                     update_anylog = True
                 # Deploy Postgres is checked
@@ -155,87 +147,81 @@ class DeploymentViews:
                 # Deploy AnyLog
                 if status is True:
                     status, errors = docker_deployment.deploy_anylog_container(env_params=env_params, timezone='utc',
-                                                                               docker_password=docker_passwd,
+                                                                               docker_password=docker_password,
                                                                                update_anylog=update_anylog)
                     if errors is not []:
                         for error in errors:
                             messages.append(error)
 
         deploy_config = forms.DeployAnyLog()
-        if len(messages) != 0:
-            return render(request, 'deploy_anylog.html', {'form': deploy_config, 'node_reply': messages})
-        return render(request, 'deploy_anylog.html', {'form': deploy_config})
+        return render(request, 'deploy_anylog.html', {'form': deploy_config, 'node_reply': messages})
 
     def base_configs(self, request)->HttpResponse:
         """
-        For a new deployment, get the build and node type respectively
+        Build and node_tye setup
         :HTML file:
             base_configs.html
         :args:
             request:django.core.handlers.wsgi.WSGIRequest - type of request against the form
         :params:
-            messages:list - list of error messages
+            basic_config:forms.BasicInfo - call to correlated forms
         :redirect:
-            --> NODE_TYPE == 'none' - empty-node-configs/
-            --> NODE_TYPE != 'None' - general-configs/
-            --> stay if there's an error || nothing is set
+            - if NODE_TYPE == none: empty-node-configs
+            - if NODE_TYPE != none and NODE_TYPE != '': general-configs
+            - other: stay
         """
-        messages = []
-        if request.meth.POST == 'POST':
-            basic_configs = forms.BaseInfo(request.POST)
-            if basic_configs.is_valid():
+        if request.method == 'POST':
+            basic_config = forms.BaseInfo(request.POST)
+            if basic_config.is_valid():
                 self.env_params['general']['build'] = request.POST.get('build')
                 self.env_params['general']['node_type'] = request.POST.get('node_type')
-                if self.env_params['general']['build'] == '':
-                    messages.append('Missing BUILD information')
-                if self.env_params['general']['node_type'] == '':
-                    messages.append('Missing NODE_TYPE information')
-
-        basic_configs = forms.BaseInfo()
-        if len(messages) > 0:
-            return render(request, "base_configs.html", {'form': basic_configs, 'node_reply': messages})
-        elif self.env_params['general']['node_type'] == 'none':
-            return HttpResponseRedirect('../empty-node-configs/')
-        elif self.env_params['general']['node_type'] not in ['', 'none'] and self.env_params['general']['build'] != '':
-            return HttpResponseRedirect('../general-configs/')
+                if self.env_params['general']['node_type'] == 'none':
+                    return HttpResponseRedirect('../none-configs/')
+                else:
+                    return HttpResponseRedirect('../general-configs/')
         else:
-            return render(request, "base_configs.html", {'form': basic_configs, 'node_reply': messages})
+            basic_config = forms.BaseInfo()
+            return render(request, 'base_configs.html', {'form': basic_config})
 
     def none_configs(self, request)->HttpResponse:
         """
-        Deployment questions for an empty node
+        Configuration options for an empty node
+        :HTML file:
+            empty_node_configs.html
         :args:
             request:django.core.handlers.wsgi.WSGIRequest - type of request against the form
         :params:
+            db_info:dict - database connection information
+                - username
+                - address
+                - password
             messages:list - list of error messages
-            db_conn_info:dict - object for db connection information
-            empty_configs:forms.NoneConfigs - call to correlated form
-        :redirect:
+            none_config:forms.NoneConfig - call to form questions related to node of type None
         """
-        status = False
-        messages = []
-        db_conn_info = {'db_user': '', 'db_addr': '', 'db_pass': ''}
+        db_info = {
+            'db_user': None,
+            'db_addr': None,
+            'db_pass': None
+        }
+        none_config = forms.NoneConfigs()
 
         if request.method == 'POST':
-            empty_configs = forms.NoneConfigs()
-            if empty_configs.is_valid():
+            none_config = forms.NoneConfigs(request.POST)
+            if none_config.is_valid():
                 self.env_params['general']['node_name'] = request.POST.get('node_name')
                 self.env_params['database']['db_type'] = request.POST.get('db_type')
                 self.env_params['database']['db_port'] = request.POST.get('db_port')
-                for option in list(db_conn_info.keys()):
-                    if request.POST.get(option) != '':
-                        db_conn_info[option] = request.POST.get(option)
-                if all(db_conn_info[key] != '' for key in list(db_conn_info.keys())):
-                    self.env_params['database']['db_user'] = '%s@%s:%s' % (db_conn_info['db_user'],
-                                                                           db_conn_info['db_addr'],
-                                                                           db_conn_info['db_pass'])
+                for param in db_info:
+                    db_info[param] = request.POST.get(param)
+                if all(db_info[param] != '' for param in db_info):
+                    self.env_params['database']['db_user'] = '%s@%s:%s' % (db_info['db_user'], db_info['db_addr'],
+                                                                           db_info['db_pass'])
+                return HttpResponseRedirect('../deploy-anylog/')
 
-                else:
-                    messages.append('Missing one or more parameters for database connection information')
-                if len(messages) > 0:
-                    return render(request, 'empty_node_configs.html', {'form': db_configs, 'node_reply': messages})
-                else:
-                    return HttpResponseRedirect('deploy-anylog/')
+        return render(request, 'empty_node_configs.html', {'form': none_config})
 
-        db_configs = forms.NoneConfigs()
-        return render(request, 'empty_node_configs.html', {'form': db_configs})
+
+
+
+
+
