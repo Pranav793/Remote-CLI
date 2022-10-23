13ba6488390a118234bb5aaea0a1f77ea6aede2b
Moshe <moshe@anylog.co>
2022-10-22 19:44:44 -0700

    adding location rectengales

--- anylog_query/views.py
+++ anylog_query/views.py
@@ -264,6 +264,7 @@ def client_processes(request, client_button):
 
     selection_output = False
     get_columns = None
+    get_descr = None
     send_button = request.POST.get("Send")
 
     # Check the form is submitted or not
@@ -274,14 +275,14 @@ def client_processes(request, client_button):
         if len(user_cmd) > 5 and user_cmd[:4].lower() == "sql ":
             query_result = True
 
-            user_cmd, selection_output, get_columns, descr_info = get_additional_instructions(user_cmd)
+            user_cmd, selection_output, get_columns, get_descr = get_additional_instructions(user_cmd)
         else:
             query_result = False
 
         # Process the command
         output = process_anylog(request, user_cmd)        # SEND THE COMMAND TO DESTINATION NODE
 
-        return print_network_reply(request, query_result, output, selection_output, get_columns)
+        return print_network_reply(request, query_result, output, selection_output, get_columns, get_descr)
 
     else:
         # Display the html form
@@ -546,13 +547,14 @@ def process_anylog(request, user_cmd):
 # Option 2 - a table
 # Option 3 - text
 # -----------------------------------------------------------------------------------
-def print_network_reply(request, query_result, data, selection_output, get_columns):
+def print_network_reply(request, query_result, data, selection_output, get_columns, get_descr):
     '''
     request - the form info
     query_result - a True/False value representing SQL query data set returned
     data - the query or command result
     selection_output - user issued a SQL statement with "> selection" at the end - indicating output to a selection table
     get_columns - the name of the columns that includes the IP, Port, dbms name and file name to retrieve he file
+     get_descr - Additional info to add to the returned blobs data
     '''
 
     select_info = {}
@@ -587,7 +589,7 @@ def print_network_reply(request, query_result, data, selection_output, get_colum
             if selection_output:
                 # Show as a selection table
 
-                return json_to_selection_table(request, select_info, policies, get_columns)
+                return json_to_selection_table(request, select_info, policies, get_columns, get_descr)
             else:
                 # Reply was a JSON policies or a query replied in JSON
                 data_list = []
@@ -617,12 +619,13 @@ def print_network_reply(request, query_result, data, selection_output, get_colum
 # Output to selection table
 # Change the query reply from JSON to selection table format and call the report
 # -----------------------------------------------------------------------------------
-def json_to_selection_table(request, select_info, policies, get_columns):
+def json_to_selection_table(request, select_info, policies, get_columns, get_descr):
     # Show as a selection table
     '''
     select_info - info directing the page
     policies - the data returned from the network
     id_column - the name of the column that includes the file name
+    get_descr - Additional info to add to the returned blobs data
     '''
     needed_columns = ["+ip@", "+port@", "+dbms@", "+table@", "+file@"]
 
