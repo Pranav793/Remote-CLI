103fcf7882e75d09325067b7a95b5fa202680939
Ori Shadmon <oshadmon@gmail.com>
2023-08-29 20:21:05 -0700

    Docker

--- remote_cli.sh
+++ remote_cli.sh
@@ -9,6 +9,6 @@ docker run -p ${CLI_PORT}:${CLI_PORT} --name remote-cli \
    -e CLI_PORT=${CLI_PORT} \
    -v remote-cli:/app/Remote-CLI/anylog_query/static/json \
    -v remote-cli-keys:/app/Remote-CLI/anylog_query/static/pem \
-   --rm  -it --detach-keys="ctrl-d" anylogco/remote-cli:latest
+   --rm  -it --detach-keys="ctrl-d" test:latest
    
 
